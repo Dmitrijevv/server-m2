76d825b76e92683c1d8874f8ecd6be71d084220e
Dmitrijevv <dmitrijevv@icloud.com>
2023-05-06 10:17:01 +0200

    first commit8

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,5 @@
 const Router = require('express');
 const PostControllers = require('../controllers/postControllers');
-const ProductControllers = require('../controllers/productControllers.js');
 const LinkWebControllers = require('../controllers/LinkWebControllers.js');
 const RegistrationControllers = require('../controllers/registrationControllers.js');
 const router = new Router();
@@ -12,12 +11,6 @@ router.get('/posts', PostControllers.getAll)
 router.get('/posts/:id', PostControllers.getOne)
 router.put('/posts', PostControllers.update)
 router.delete('/posts/:id', PostControllers.delete)
-
-router.post('/products', ProductControllers.create)
-router.get('/products', ProductControllers.getAll)
-router.get('/products/:id', ProductControllers.getOne)
-router.put('/products', ProductControllers.update)
-router.delete('/products/:id', ProductControllers.delete)
 
 router.post('/linkwebs', LinkWebControllers.create)
 router.get('/linkwebs', LinkWebControllers.getAll)
